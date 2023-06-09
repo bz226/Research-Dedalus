8dd6be23eab6127e4d36e93c87dd762201117dbe
Bobby Bao <zb2113@log-2.hpc.nyu.edu>
2023-06-09 18:30:35 -0400

    1

--- ivp_2d_rayleigh_benard/rayleigh_benard.py
+++ ivp_2d_rayleigh_benard/rayleigh_benard.py
@@ -65,8 +65,6 @@ lift_basis = zbasis.derivative_basis(1)
 lift = lambda A: d3.Lift(A, lift_basis, -1)
 grad_u = d3.grad(u) + ez*lift(tau_u1) # First-order reduction
 grad_b = d3.grad(b) + ez*lift(tau_b1) # First-order reduction
-dz= lambda A: d3.Differentiate(A, coords['z'])
-dx= lambda A: d3.Differentiate(A, coords['x'])
 
 # Problem
 # First-order form: "div(f)" becomes "trace(grad_f)"
@@ -78,7 +76,7 @@ problem.add_equation("dt(u) - nu*div(grad_u) + grad(p) - b*ez + lift(tau_u2) = -
 problem.add_equation("b(z=0) = Lz")
 problem.add_equation("u(z=0) = 0")
 problem.add_equation("b(z=Lz) = 0")
-problem.add_equation("dz(u)(z=Lz) = 0")
+problem.add_equation("u(z=Lz) = 0")
 problem.add_equation("integ(p) = 0") # Pressure gauge
 
 # Solver
