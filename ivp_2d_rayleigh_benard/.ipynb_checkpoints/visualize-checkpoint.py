904a2bd88e639a6b01aa65fd279c479ce813b328
Bobby Bao <zb2113@log-2.hpc.nyu.edu>
2023-06-16 22:03:38 -0400

    right commit

---
+++ ivp_2d_rayleigh_benard/.ipynb_checkpoints/visualize-checkpoint.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as Animation
+from PIL import Image
+import os
+from os import listdir
+
+folder_dir = 'frames'
+
+# Get a list of PNG file paths from the directory
+file_paths = [os.path.join(folder_dir, f) for f in listdir(folder_dir) if os.path.isfile(os.path.join(folder_dir, f)) and f.endswith('.png')]
+
+# Sort the list of file paths
+file_paths.sort()
+
+# Read the images using PIL
+imgs = [Image.open(f) for f in file_paths]
+
+fig = plt.figure()
+
+# Wrap each image in a list to create a list of sequences of artists
+imgs = [[plt.imshow(img, animated=True)] for img in imgs]
+
+ani = Animation.ArtistAnimation(fig, imgs, interval=100, blit=True, repeat_delay=1000)
+
+# Save the animation to a file
+ani.save('dynamic_images.mp4')
+
+plt.show()
