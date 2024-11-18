c655b5cc1435f6df407682ae64b64042e2f41d3d
Bobby Bao <zb2113@log-2.hpc.nyu.edu>
2024-11-18 15:17:53 -0500

    1# with '#' will be ignored, and an empty message aborts the commit.

--- 2d-AR=64/.ipynb_checkpoints/mountain3-checkpoint.py
+++ 2d-AR=64/.ipynb_checkpoints/mountain3-checkpoint.py
@@ -9,13 +9,13 @@ import matplotlib
 import re
 
 # Parameters
-Lx, Lz = 4,1
-Nx, Nz = 512, 128
-Ra_M = 1e5
+Lx, Lz = 2,1
+Nx, Nz = 1024,512
+Ra_M = -1e12
 # D_0 = 0
 # D_H = 1/3
-M_0 = 1
-M_H = 0
+M_0 = 0
+M_H = 1
 N_s2=4/3
 Qrad=0.0028
 gamma=100
@@ -23,13 +23,13 @@ gamma_s=1
 
 Prandtl = 1
 dealias = 3/2
-stop_sim_time = 50
+stop_sim_time = 500
 timestepper = d3.RK222
 max_timestep = min(0.125, 0.25/gamma)
 dtype = np.float64
 
 mask_dir= "/home/zb2113/Research-Dedalus/2d-AR=64/masks"
-save_dir= "/scratch/zb2113/DedalusData/mountain"
+save_dir= "/scratch/zb2113/DedalusData/mountain3"
 # %%
 # Bases
 coords = d3.CartesianCoordinates('x','z')
@@ -67,7 +67,8 @@ tau_t1 = dist.Field(name='tau_t1', bases=xbasis)
 tau_t2 = dist.Field(name='tau_t2', bases=xbasis)
 F = dist.Field(name='F', bases=(xbasis,zbasis))
 M_s = dist.Field(name='M_s', bases=(xbasis,zbasis))
-u_s = dist.Field(name='u_s', bases=(xbasis,zbasis))
+u_fix = dist.Field(name='u_fix', bases=(xbasis,zbasis))
+u_period = dist.Field(name='u_period', bases=(xbasis,zbasis))
 
 # Substitutions    
 #Kuo_Bretherton Equilibrium
@@ -88,14 +89,17 @@ ex,ez = coords.unit_vector_fields(dist)
 lift_basis = zbasis.derivative_basis(1)
 lift = lambda A: d3.Lift(A, lift_basis, -1)
 
-B_op = (np.absolute(D - M - N_s2*Z)+ M + D - N_s2*Z)/2
-lq = B_op/2 + np.absolute(B_op)
+# B_op = (np.absolute(D - M - N_s2*Z)+ M + D - N_s2*Z)/2
+# lq = B_op/2 + np.absolute(B_op)
 
 # F=(max((Lx/10-x)/(Lx/10),0)+max((-Lx+Lx/10+x)/(Lx/10),0))
 
 F['g']= (Lx/10-x)/(Lx/10)/2 +np.absolute((Lx/10-x)/(Lx/10))/2 + (-Lx+Lx/10+x)/(Lx/10)/2 + np.absolute((-Lx+Lx/10+x)/(Lx/10))/2
-M_s['g']=z
-u_s['g']=10*z
+M_s['g']=z/Lz
+u_fix['g']=0.02
+u_period['g']=3*z/Lz
+# u_s = u_fix + np.sin(time*2*np.pi/5) * u_period
+u_s = u_fix
 
 max = lambda A,B: (abs(A-N_s2*z-B)+A-N_s2*z+B)/2
 eva = lambda A: A.evaluate()
@@ -155,13 +159,14 @@ problem.add_equation("trace(grad_u) + tau_p= 0")
 # problem.add_equation("dt(M) - kappa*div(grad_M) + lift(tau_M2) = - u@grad(M) - mask*gamma*(M-M_0)  -F*sponge*gamma_s*(M-(Lz-Z)/Lz)")
 # problem.add_equation("dt(M) - kappa*div(grad_M) + lift(tau_M2) = - u@grad(M) - mask*gamma*(M-M_0)  -sponge*gamma*(M-(Z-Lz)/Lz)")
 # problem.add_equation("dt(M) - kappa*div(grad_M) + lift(tau_M2) = - u@grad(M) - mask*gamma*(M-M_0)  -sponge*gamma*M")
-problem.add_equation("dt(M) - kappa*div(grad_M) + lift(tau_M2) = - u@grad(M)  -F*sponge*gamma_s*(M-M_s)")
+problem.add_equation("dt(M) - kappa*div(grad_M) + lift(tau_M2) = - u@grad(M)- sponge*gamma_s*(M-M_s)")
 # problem.add_equation("dt(u) - nu*div(grad_u) + grad(p)  + lift(tau_u2) -M*ez = - u@grad(u) - mask*gamma*u- F*sponge*gamma_s*(u-10/1*Z*ex)")
 # problem.add_equation("dt(u) - nu*div(grad_u) + grad(p)  + lift(tau_u2) -M*ez = - u@grad(u) - mask*gamma*u- sponge*gamma*(u-10/1*Z*ex)")
 # problem.add_equation("dt(u) - nu*div(grad_u) + grad(p)  + lift(tau_u2) -M*ez = - u@grad(u) - mask*gamma*u- sponge*gamma*(u-5/1*Z*ex)")
-problem.add_equation("dt(u) - nu*div(grad_u) + grad(p)  + lift(tau_u2) -M*ez = - u@grad(u) - F*sponge*gamma_s*(u-u_s*ex)")
+problem.add_equation("dt(u) - nu*div(grad_u) + grad(p)  + lift(tau_u2) -M*ez = - u@grad(u) - mask*gamma*u- sponge*gamma_s*(u-u_s*ex)")
 problem.add_equation("dt(time) = 1 ")
-problem.add_equation("u(z=0) = 0")
+problem.add_equation("uz(z=0) = 0")
+problem.add_equation("dz(ux)(z=0)=0")
 problem.add_equation("uz(z=Lz) = 0")
 problem.add_equation("dz(ux)(z=Lz)=0")
 problem.add_equation("M(z=0) = M_0")
@@ -181,13 +186,13 @@ solver.stop_sim_time = stop_sim_time
 
 # %%
 # Initial condition
-M.fill_random('g', seed=28, distribution='normal', scale=1e-3) # Random noise
-M['g'] *= z * (Lz - z) # Damp noise at walls
-M['g'] += (M_H-M_0)*z+M_0 # Add linear background
+# M.fill_random('g', seed=28, distribution='normal', scale=1e-3) # Random noise
+# M['g'] *= z * (Lz - z) # Damp noise at walls
+M['g'] += (M_H-M_0)*z/Lz+M_0 # Add linear background
 M.change_scales(dealias)
 
-# M['g'] *=(1-mask['g']) # Apply mask
-M['g'] *= (1-sponge['g']) # Apply sponge
+M['g'] *=(1-mask['g']) # Apply mask
+# M['g'] *= (1-sponge['g']) # Apply sponge
 time['g']=0
 
 # M.change_scales(1)
@@ -196,16 +201,21 @@ time['g']=0
 
 # %%
 # Analysis
-snapshots = solver.evaluator.add_file_handler(save_dir+'/snapshots',sim_dt=0.25, max_writes=1)
+snapshots = solver.evaluator.add_file_handler(save_dir+'/snapshots',sim_dt=5, max_writes=1)
 snapshots.add_tasks(solver.state,layout='g')
 snapshots.add_task(u@u, layout='g', name='u square')
 snapshots.add_task(u@ez, layout='g', name='uz')
 snapshots.add_task(u@ex, layout='g', name='ux')
 snapshots.add_task(F*sponge, layout='g', name='Fsponge')
+snapshots.add_task(dz(M), name='dzM')
+snapshots.add_task(dx(uz), name='dxuz')
+snapshots.add_task(dx(M), name='dxM')
+snapshots.add_task(dz(dz(M)), name='dzdzM')
+snapshots.add_task(dx(dx(M)), name='dxdxM')
 
 
 
-maskcheck = solver.evaluator.add_file_handler(save_dir+'/maskcheck',sim_dt=5, max_writes=1)
+maskcheck = solver.evaluator.add_file_handler(save_dir+'/maskcheck',sim_dt=100, max_writes=1)
 maskcheck.add_task(mask, layout='g', name='mask')
 maskcheck.add_task(sponge, layout='g', name='sponge')
 
